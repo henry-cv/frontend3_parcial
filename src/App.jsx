18b301a6738a8f9d63c97ee61e4d76fe36fb339b
Henry Cuesta <141352590+henry-cv@users.noreply.github.com>
2024-11-07 18:29:38 -0500

    Parcial completo, con las consignas solicitadas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,48 @@
+import { useState } from "react";
 import "./App.css";
 import Card from "./components/Card";
 
 function App() {
+  const [cuenta, setCuenta] = useState({
+    nombre: "",
+    pelicula: "",
+  });
+  const [mostrar, setMostrar] = useState(false);
+  const [error, setError] = useState(false);
+
+  const manejaFormulario = (e) => {
+    e.preventDefault();
+    const exp1 = /^[a-zA-Záéíóúñ]{3,}$/;
+    const exp2 = /^\w{6,}$/;
+    console.log(exp1.test(cuenta.nombre));
+    console.log(exp2.test(cuenta.pelicula));
+    if (exp1.test(cuenta.nombre.trim()) && exp2.test(cuenta.pelicula.trim())) {
+      setMostrar(true);
+      setError(false);
+    } else {
+      setError(true);
+      setMostrar(false);
+    }
+  };
+
   return (
     <div className="App">
       <h1>Carga de estudiantes</h1>
-      <form></form>
-      <Card />
+      <form onSubmit={manejaFormulario}>
+        <input
+          type="text"
+          placeholder="Ingresa tu nombre"
+          onBlur={(e) => setCuenta({ ...cuenta, nombre: e.target.value })}
+        />
+        <input
+          type="text"
+          placeholder="Cuál es tu película favorita"
+          onBlur={(e) => setCuenta({ ...cuenta, pelicula: e.target.value })}
+        />
+        <input type="submit" value="Enviar" />
+      </form>
+      {mostrar && <Card nombre={cuenta.nombre} pelicula={cuenta.pelicula} />}
+      {error && <p>Por favor chequea que la información sea correcta</p>}
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,14 @@
-const Card = () => {
-  return <div>Hola, soy una tarjeta</div>;
+/* eslint-disable react/prop-types */
+const Card = ({ nombre, pelicula }) => {
+  //console.log(props);
+  //console.log(props.nombre, props.pelicula);
+  console.log(`nombre: ${nombre}, pelicula: ${pelicula}`);
+  return (
+    <div>
+      <h4>Hola: {nombre}!</h4>
+      <p>tu película favorita es: {pelicula}</p>
+    </div>
+  );
 };
 
 export default Card;
